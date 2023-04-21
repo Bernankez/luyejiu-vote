d7a0ec7ee6a6313ae27ca437b087dc821bd0ea9c
cole <bernankez@qq.com>
2023-04-22 00:52:17 +0800

    chore: vite-ssg

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div>homepage</div>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { useHead } from "@vueuse/head";
+
+useHead({
+  title: "鹿野灸忏悔回投票机",
+  meta: [
+    {
+      name: "description",
+      content: "鹿野灸忏悔回投票机",
+    },
+  ],
+});
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  transition: filter 300ms;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

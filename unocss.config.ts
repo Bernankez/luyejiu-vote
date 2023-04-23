6c33d8bd526100ff2e9a5fd9cfcbc59454567b9e
cole <bernankez@qq.com>
2023-04-24 02:54:44 +0800

    chore: fix unocss config

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, presetIcons, presetUno, transformerDirectives } from "unocss";
-import { primary } from "@/styles/color";
+import { primary } from "./src/styles/color";
 
 export default defineConfig({
   presets: [presetUno(), presetIcons()],

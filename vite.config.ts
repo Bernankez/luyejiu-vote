2f51663178ff077f4878d5bd9b86bb77b7f15a52
cole <bernankez@qq.com>
2023-04-23 21:55:41 +0800

    chore: auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,32 @@ import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import UnoCSS from "unocss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/,
+        /\.vue$/, /\.vue\?vue/,
+      ],
+      imports: [
+        "vue",
+        "@vueuse/core",
+      ],
+      dirs: [
+        "src/composables",
+      ],
+      vueTemplate: true,
+    }),
+    Components({
+      dirs: ["src/components"],
+      deep: true,
+    }),
   ],
   resolve: {
     alias: {

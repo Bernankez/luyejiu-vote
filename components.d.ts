4784382bb47ad7d230dadb182962f8cb5289ea01
cole <bernankez@qq.com>
2023-04-25 01:24:08 +0800

    chore: remove unused

--- components.d.ts
+++ components.d.ts
@@ -9,23 +9,16 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Button: typeof import('./src/components/ui/Button.vue')['default']
     ConfigProvider: typeof import('./src/components/common/ConfigProvider.vue')['default']
-    copy: typeof import('./src/components/common/Header copy.vue')['default']
     Header: typeof import('./src/components/common/Header.vue')['default']
-    'Header copy': typeof import('./src/components/common/Header copy.vue')['default']
     LBadgeText: typeof import('./src/components/ui/LBadgeText.vue')['default']
     LButton: typeof import('./src/components/ui/LButton.vue')['default']
     NBadge: typeof import('naive-ui')['NBadge']
-    NButton: typeof import('naive-ui')['NButton']
     NColorPicker: typeof import('naive-ui')['NColorPicker']
-    NFormItem: typeof import('naive-ui')['NFormItem']
     NInput: typeof import('naive-ui')['NInput']
     NModal: typeof import('naive-ui')['NModal']
     SettingDialog: typeof import('./src/components/common/SettingDialog.vue')['default']
-    Tooltip: typeof import('./src/components/ui/Tooltip.vue')['default']
     VoteBar: typeof import('./src/components/common/VoteBar.vue')['default']
     VotePanel: typeof import('./src/components/common/VotePanel.vue')['default']
-    VoteResult: typeof import('./src/components/common/VoteResult.vue')['default']
   }
 }

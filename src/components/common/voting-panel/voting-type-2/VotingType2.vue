9ffd06ebcb7e909d9714f94b8d6b18d72e997719
cole <bernankeic@gmail.com>
2023-09-08 14:40:07 +0800

    feat: complete VotingType2

--- src/components/common/voting-panel/voting-type-2/VotingType2.vue
+++ src/components/common/voting-panel/voting-type-2/VotingType2.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="machine relative box-border rounded-4 p-10 p-t-0">
+  <div class="machine relative box-border w-100 overflow-hidden rounded-4 p-10 p-t-0">
+    <div class="relief absolute left-3 top-1.5 select-none font-[AWAVE_Regular]">
+      luyejiu
+    </div>
     <div class="m-b-3 m-t-4 flex justify-center text-yellow-800">
       <VT2LED state="on" light :color-map="{ on: 'rgb(22, 163, 74)', off: 'rgb(229, 57, 53)', warn: 'rgb(249, 168, 37)' }" />
     </div>
@@ -9,19 +12,19 @@
         <VT2ScreenItem class="text-red" animation icon="i-fluent-emoji-high-contrast:smiling-face-with-horns" :count="123" />
       </VT2LCD>
     </div>
-    <div class="m-t-3 box-border w-full flex justify-between rounded-full">
-      <div class="flex flex-1 justify-center">
+    <div class="m-t-3 box-border w-full flex justify-evenly rounded-full">
+      <div class="button-track w-35% flex justify-end rounded-full p-1">
         <VT2Button class="relative h-12! w-12! text-8!" theme="light">
-          <div class="absolute left-0 p-r-5 text-10 font-[Kenney_Pixel] text-dark -translate-x-100%">
+          <div class="pointer-events-none absolute left-0 select-none p-r-5 text-10 font-[Kenney_Pixel] text-dark -translate-x-100%">
             1
           </div>
           <div class="i-ion:caret-up"></div>
         </VT2Button>
       </div>
-      <div class="flex flex-1 justify-center">
+      <div class="button-track w-35% flex justify-start rounded-full p-1">
         <VT2Button class="relative h-12! w-12! text-8!">
           <div class="i-ion:caret-down"></div>
-          <div class="absolute right-0 translate-x-100% p-l-5 text-10 font-[Kenney_Pixel] text-dark">
+          <div class="pointer-events-none absolute right-0 translate-x-100% select-none p-l-5 text-10 font-[Kenney_Pixel] text-dark">
             2
           </div>
         </VT2Button>
@@ -29,8 +32,8 @@
     </div>
     <div class="flex justify-center">
       <VT2Button class="h-24! w-24! text-14!">
-        <div class="i-ion:play translate-x-1"></div>
-        <div class="i-ion:stop translate-x-1"></div>
+        <!-- <div class="i-ion:play translate-x-1"></div> -->
+        <div class="i-ion:stop"></div>
       </VT2Button>
     </div>
     <div class="flex">
@@ -41,7 +44,7 @@
       </div>
       <div class="flex flex-1 justify-center"></div>
     </div>
-    <img :src="Sticker" class="absolute bottom-0 right-0 w-10 object-scale-down" />
+    <img :src="Sticker" class="absolute bottom-0 right-0 w-50 translate-x-7 translate-y-7 object-scale-down -rotate-30" />
   </div>
 </template>
 
@@ -50,6 +53,10 @@ import Sticker from "@/assets/images/sticker.png";
 </script>
 
 <style scoped>
+.relief {
+  filter: drop-shadow(1px -1px 0 #eee);
+}
+
 .machine {
   border: 2px solid rgb(153 153 153);
   background-image: linear-gradient(to right, #adb9b3, #bec2c1 10%);
